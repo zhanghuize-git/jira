28d173c809c6c3051b767d0b5ba0e06a0e50faac
zhanghuize <56755115+zhanghuize-git@users.noreply.github.com>
2022-11-23 19:39:13 +0800

    chore: add debounce

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import qs from 'qs';
 import { SearchPanel } from "./search-panel";
 import { List } from "./list";
-import { cleanObject } from 'utils';
+import { cleanObject, useMount, useDebounce } from 'utils';
 
 const apiUrl = process.env.REACT_APP_API_URL;
 console.log('apiUrl',apiUrl);
@@ -13,23 +13,24 @@ export const ProjectListScreen = () => {
     name: '',
     personId: ''
   })
+  const debounceParam = useDebounce(param, 2000);
   const [users, setUsers] = useState([])
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debounceParam))}`).then(async response => {
       if (response.ok) {
         setList(await response.json())
       }
     })
-  }, [param])
+  }, [debounceParam])
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if (response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [])
+  })
 
   return <div>
     <SearchPanel

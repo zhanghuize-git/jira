5609cd36824db8e897f5ddc0b97ea615ad1623f7
zhanghuize <56755115+zhanghuize-git@users.noreply.github.com>
2022-11-26 14:52:30 +0800

    chore: add jira-dev

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { loadDevTools } from "jira-dev-tool";
 
 import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+loadDevTools(() =>
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
 );
 
 // If you want to start measuring performance in your app, pass a function

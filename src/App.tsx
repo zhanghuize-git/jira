93abfccd1ef75693fdfe6f6a8d4ae877b42e26a7
zhanghuize <56755115+zhanghuize-git@users.noreply.github.com>
2022-11-26 14:41:43 +0800

    chore: add middleware

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { ProjectListScreen } from "screens/project-list";
+import { LoginScreen } from "screens/login";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen />
+      {/* <ProjectListScreen /> */}
+      <LoginScreen />
     </div>
   );
 }
